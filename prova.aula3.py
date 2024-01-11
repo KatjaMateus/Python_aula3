7e445d1861cde37bcaef94dd52d7ce9d2edd55d7
KatjaMateus <145811394+KatjaMateus@users.noreply.github.com>
2024-01-10 22:18:16 -0300

    Update prova.aula3.py

--- prova.aula3.py
+++ prova.aula3.py
@@ -1,13 +1,31 @@
-alunado = {1234  : "Diana Soares", 3456 : "João Gomes", 2345 : "Marcos Saraiva"}
+alunos = {1234  : "Diana Soares", 3456 : "João Gomes", 2345 : "Marcos Saraiva"}
 
-while True:    
-    matricula=int(input("Digite número de matrícula: "))
-    if matricula == 0000:
-        break        
-    nome=str(input("Digite o nome do aluno: "))
-    alunado.update([[matricula, nome]])
-print(alunado)
-
-deletar = int(input("Digite o número de matrícula a ser deletado: "))
-del alunado[deletar]
-print(alunado)
+while True:
+    escolha = int(input("""
+        Escolha uma opção:
+        1 = visualizar alunos
+        2 = adicionar alunos
+        3 = remover alunos
+        4 = sair
+                    
+      """))
+    if escolha == 1:    
+        print(alunos)
+    elif escolha == 2:
+        while True:    
+            matricula=int(input("Digite número de matrícula: "))
+            if matricula == 0:
+                break        
+            nome=str(input("Digite o nome do aluno: "))
+            alunos.update([[matricula, nome]])
+        print(alunos)      
+    elif escolha == 3:
+        print(alunos)
+        deletar = int(input("Digite o número de matrícula do aluno a ser removido: "))
+        del alunos[deletar]
+        print(f"A matrícula {deletar} foi removido com sucesso")       
+    elif escolha == 4:
+        print("Programa encerrado")
+        break
+    else:
+        print("Opção inválida")
